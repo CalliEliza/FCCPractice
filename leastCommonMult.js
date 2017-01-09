9bbff325ff38754c27f646e1992a7d16e11dfc49
callieliza <calidesigns42@gmail.com>
2017-01-08 21:38:35 -0500

    completed algortihm

--- leastCommonMult.js
+++ leastCommonMult.js
@@ -20,12 +20,25 @@ function getArray(a,b) { //returns an array of values b/t two given points
 function smallestCommons(arr) {
     var arrayList = getArray(arr[0],arr[1]);
     var lcm = 0;
-    var counter = arrayList.length-1;
+    var len = arrayList.length;
+    var counter = arrayList.length;
     while (lcm === 0) {
-
+        var trueCount = 0;
+        for (var i=0;i<len;++i) {
+            if (counter % arrayList[i] === 0) {
+                ++trueCount;
+            }
+            if (trueCount == arrayList.length) {
+                lcm = counter;
+            }
+        }
+        ++counter;
     }
+    //console.log(lcm);
     return lcm;
 }
 
-smallestCommons([1,5]);
-//getArray(15,5);
+smallestCommons([1,5]); // should return 60
+smallestCommons([5, 1]); // should return 60.
+smallestCommons([1, 13]); // should return 360360.
+smallestCommons([23, 18]); // should return 6056820.
