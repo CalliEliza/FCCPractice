be4f8f5908a4f41e3713b2a1ae519150f052c863
callieliza <calidesigns42@gmail.com>
2017-01-09 10:58:39 -0500

    working on spinal case, removing _

--- .idea/spinalCase.js
+++ .idea/spinalCase.js
@@ -6,18 +6,30 @@
 
 function spinalCase(str) {
     var spinalCase;
-    if () {}
-    var lowerCase = str.toLowerCase();
+    if (str.indexOf(' ') != -1) {
+        var lowerCase = str.toLowerCase();
+        spinalCase = lowerCase.split(' ').join('-');
+        console.log("final ans, with spaces "+spinalCase);
+        return spinalCase;
+    } else if (str.indexOf(' ') == -1) {
+        var middleStep = str.replace(/([A-Z])/g, '-$1');
+        //console.log("middle step, no spaces "+middleStep);
+        spinalCase = middleStep.toLowerCase();
+        console.log("no spaces "+spinalCase);
+    } else if (str.indexOf('_') != -1) {
+        var midStep = str.split('_').join('-');
+        console.log(midStep);
+    }
+
     //console.log(lowerCase);
-    spinalCase = lowerCase.split(' ').join('-');
-    console.log(spinalCase);
+    //console.log(spinalCase);
     return spinalCase;
 }
 
-spinalCase('This Is Spinal Tap');
+//spinalCase('This Is Spinal Tap');
 
 spinalCase("This Is Spinal Tap"); //should return "this-is-spinal-tap".
 spinalCase("thisIsSpinalTap"); //should return "this-is-spinal-tap".
 spinalCase("The_Andy_Griffith_Show"); //should return "the-andy-griffith-show".
-spinalCase("Teletubbies say Eh-oh"); //should return "teletubbies-say-eh-oh".
-spinalCase("AllThe-small Things"); //should return "all-the-small-things".
+//spinalCase("Teletubbies say Eh-oh"); //should return "teletubbies-say-eh-oh".
+//spinalCase("AllThe-small Things"); //should return "all-the-small-things".
