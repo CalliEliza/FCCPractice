c91849817e163b610e5e73b8018a2b51f2697ad2
callieliza <calidesigns42@gmail.com>
2017-01-08 21:25:05 -0500

    making updates to algorithm

--- leastCommonMult.js
+++ leastCommonMult.js
@@ -2,29 +2,29 @@
  * Created by chewy on 1/8/17.
  */
 function smallestCommons(arr) {
-    var item1 = arr[0];
-    var item2 = arr[1];
+    var arrayList = getArray(arr[0],arr[1]);
     var lcm = 0;
-    var counter1 = item1 +1;
-    var counter2 = item2 +1;
+    var counter = arrayList.length-1;
     while (lcm === 0) {
-        if (counter1 % item1 === 0) {
-            if (counter1 % item2 === 0) {
-                lcm = counter1;
-            } else { ++counter1; }
-        } else {
-            ++counter1;
-        }
-        if (counter2 % item2 === 0) {
-            if (counter2 % item1 === 0) {
-                lcm = counter2;
-            } else { ++counter2; }
-        } else {
-            ++counter2;
-        }
+
     }
     return lcm;
 }
 
+function getArray(a,b) {
+    var arrayList = [];
+    if (a < b) {
+        for (var i = a; i <= b; ++i) {
+            arrayList.push(i);
+        }
+    } else {
+        for (var j = b;j<=a;++j) {
+            arrayList.push(j);
+        }
+    }
+    //console.log(arrayList);
+    return arrayList;
+}
 
 smallestCommons([1,5]);
+//getArray(15,5);
