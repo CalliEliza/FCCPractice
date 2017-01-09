d68c3410ec4af428fb3b73e74f93d19a6cee7956
callieliza <calidesigns42@gmail.com>
2017-01-09 10:20:56 -0500

    spinal case algorithm

---
+++ .idea/spinalCase.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chewy on 1/9/17.
+ */
+
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+    var spinalCase;
+
+    return spinalCase;
+}
+
+spinalCase('This Is Spinal Tap');
