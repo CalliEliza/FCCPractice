3f68f7a1ed209f61f2c7361fb40c9fb4dc0f74aa
callieliza <calidesigns42@gmail.com>
2017-01-09 10:33:31 -0500

    working on spinal case algorithm, not complete

--- .idea/spinalCase.js
+++ .idea/spinalCase.js
@@ -6,8 +6,18 @@
 
 function spinalCase(str) {
     var spinalCase;
-
+    if () {}
+    var lowerCase = str.toLowerCase();
+    //console.log(lowerCase);
+    spinalCase = lowerCase.split(' ').join('-');
+    console.log(spinalCase);
     return spinalCase;
 }
 
 spinalCase('This Is Spinal Tap');
+
+spinalCase("This Is Spinal Tap"); //should return "this-is-spinal-tap".
+spinalCase("thisIsSpinalTap"); //should return "this-is-spinal-tap".
+spinalCase("The_Andy_Griffith_Show"); //should return "the-andy-griffith-show".
+spinalCase("Teletubbies say Eh-oh"); //should return "teletubbies-say-eh-oh".
+spinalCase("AllThe-small Things"); //should return "all-the-small-things".
