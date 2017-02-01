b103bdf73c3a87c62243cdd846755ddf1ce5516f
callieliza <calidesigns42@gmail.com>
2017-02-01 17:29:37 -0500

    completed roman num converter

--- RomNumConv.js
+++ RomNumConv.js
@@ -2,8 +2,8 @@
 //All roman numerals answers should be provided in upper-case.
 
 function convertToRoman(num) {
-     var number = [1000,500,100,50,10,5,1];
-     var roman = ["M","D","C","L","X","V","I"];
+     var number = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+     var roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
      var len = number.length;
      var ans = '';
      for (var i =0;i<len;++i) {
@@ -18,28 +18,27 @@ function convertToRoman(num) {
 
 console.log(convertToRoman(36));
 
-//convertToRoman(2) //should return "II".
-//convertToRoman(3) //should return "III".
-//convertToRoman(4) //should return "IV".
-//convertToRoman(5) //should return "V".
-//convertToRoman(9) //should return "IX".
-//convertToRoman(12) //should return "XII".
-//convertToRoman(16) //should return "XVI".
-//convertToRoman(29) //should return "XXIX".
-//convertToRoman(44) //should return "XLIV".
-//convertToRoman(45) //should return "XLV"
-//convertToRoman(68) //should return "LXVIII"
-//convertToRoman(83) //should return "LXXXIII"
-//convertToRoman(97) //should return "XCVII"
-//convertToRoman(99) //should return "XCIX"
-//convertToRoman(500) //should return "D"
-//convertToRoman(501) //should return "DI"
-//convertToRoman(649) //should return "DCXLIX"
-//convertToRoman(798) //should return "DCCXCVIII"
-//convertToRoman(891) //should return "DCCCXCI"
-//convertToRoman(1000) //should return "M"
-//convertToRoman(1004) //should return "MIV"
-//convertToRoman(1006) //should return "MVI"
-//convertToRoman(1023) //should return "MXXIII"
-//convertToRoman(2014) //should return "MMXIV"
-//convertToRoman(3999) //should return "MMMCMXCIX"
+//console.log(convertToRoman(3)); //should return "III".
+//console.log(convertToRoman(4)); //should return "IV".
+//console.log(convertToRoman(5)); //should return "V".
+//console.log(convertToRoman(9)); //should return "IX".
+//console.log(convertToRoman(12)); //should return "XII".
+//console.log(convertToRoman(16)); //should return "XVI".
+//console.log(convertToRoman(29)); //should return "XXIX".
+//console.log(convertToRoman(44)); //should return "XLIV".
+//console.log(convertToRoman(45)); //should return "XLV"
+//console.log(convertToRoman(68)); //should return "LXVIII"
+//console.log(convertToRoman(83)); //should return "LXXXIII"
+//console.log(convertToRoman(97)); //should return "XCVII"
+//console.log(convertToRoman(99)); //should return "XCIX"
+//console.log(convertToRoman(500)); //should return "D"
+//console.log(convertToRoman(501)); //should return "DI"
+//console.log(convertToRoman(649)); //should return "DCXLIX"
+//console.log(convertToRoman(798)); //should return "DCCXCVIII"
+//console.log(convertToRoman(891)); //should return "DCCCXCI"
+//console.log(convertToRoman(1000)); //should return "M"
+//console.log(convertToRoman(1004)); //should return "MIV"
+//console.log(convertToRoman(1006)); //should return "MVI"
+//console.log(convertToRoman(1023)); //should return "MXXIII"
+//console.log(convertToRoman(2014)); //should return "MMXIV"
+//console.log(convertToRoman(3999)); //should return "MMMCMXCIX"
