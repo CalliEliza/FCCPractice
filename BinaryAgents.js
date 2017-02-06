2ccc0eaf8bf55d985fd543b126f0423385597cb8
callieliza <calidesigns42@gmail.com>
2017-02-05 19:48:26 -0500

    completed binary agents

--- BinaryAgents.js
+++ BinaryAgents.js
@@ -8,19 +8,40 @@
 
 
 // solution:
-// 1. turn string into an array of string
-// 2. convert each unicode to char code
-// 3. get corresponding letter for each code.
+// 1. convert binary to decimal
+// 2. find letter for decimal
+// 3. create new string
 
 function binaryAgent(str) {
-    let str1 = str.split(" ");
+    let str1 = str.split(" "); // converts string into array of strings
     const len = str1.length;
-    //console.log(str1);
-    for (let i =0;i<len;++i) {
-        let temp = str1[i];
-        //console.log(str1[i]);
+    // get decimal for each binary
+    let deciArray = []; // will hold decimal values
+    for (let i =0;i<len;++i) { // looping through original string
+        let temp1 = str1[i].split(""); // split string into smaller array of strings
+        //console.log(temp1);
+        let convDeci = 0;
+        let temp4 = temp1.reverse();
+        for (let j =0;j<temp1.length;++j) { // will return decimal value for each binary code
+            //console.log(temp1[j]);
+            let temp2 = temp4[j];
+            if (temp2 == 1) {
+                let temp = Math.pow(2,j);
+                //console.log(temp);
+                convDeci += temp; // adds value to variable
+            }
+        }
+        deciArray.push(convDeci);
+        //console.log(deciArray);
     }
-    return str1;
+    // loop through decimal array and get letter value for each
+    let ansString = '';
+    deciArray.forEach(function(decimal) {
+        let tempVar = String.fromCharCode(decimal);
+        ansString += tempVar;
+    });
+    console.log(ansString);
+    return ansString;
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
