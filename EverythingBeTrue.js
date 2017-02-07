06b9ad86e383ccd9a26997724018bc9039144920
callieliza <calidesigns42@gmail.com>
2017-02-07 15:05:52 -0500

    completed EverythingBeTrue

--- EverythingBeTrue.js
+++ EverythingBeTrue.js
@@ -8,35 +8,45 @@
 
 // Solution:
 // 1. get list of keys from collection
-// 2. check to see if all contain the pre value as a key
+// 2. filter list to those that have the pre key value
+// 3. count how many have truth valus
+// 4. if all have truthy values, return true otherwise return false
 
 
 function truthCheck(collection, pre) {
+    //console.log("pre: "+pre);
     let keys = Object.keys(collection);
     let isTrue = true;
+    let counter = 0;
     keys.forEach(key => {
         //console.log(collection[key]);
         let tempKey = Object.keys(collection[key]);
+        //console.log(key);
         //console.log(tempKey);
-        tempKey.filter(tKey => {
-            if (tKey == pre) {
-               // console.log(tKey);
-                if (!tKey.value) {
-                    isTrue = false;
-                }
+        const filteredKeys = tempKey.filter(tKey => {
+            return tKey == pre;
+        });
+        filteredKeys.forEach(fKey => {
+            //console.log(collection[key][pre]);
+            if (Boolean(collection[key][pre])) {
+                console.log(Boolean(collection[key][pre]));
+                ++counter;
             }
         });
     });
+    if (counter != collection.length) {
+        isTrue = false;
+    }
     console.log(isTrue);
     return isTrue;
 }
 
-//truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
 
 //truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age") //should return false.
 //truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat") //should return false
-truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat") //should return true
-truthCheck([{"single": "yes"}], "single") //should returnEverythingBeTrue.js:10 true
+// check //truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat") //should return true
+// check //truthCheck([{"single": "yes"}], "single") //should returnEverythingBeTrue.js:10 true
 //truthCheck([{"single": ""}, {"single": "double"}], "single") //should return false
 //truthCheck([{"single": "double"}, {"single": undefined}], "single") //should return false
 //truthCheck([{"single": "double"}, {"single": NaN}], "single") //should return false
