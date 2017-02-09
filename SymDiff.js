c085d6c8f3aa4f1651a1dbc2ef8aaf7a5ecfd5f3
callieliza <calidesigns42@gmail.com>
2017-02-09 11:34:24 -0500

    completed algorithm for symdiff.js

--- SymDiff.js
+++ SymDiff.js
@@ -14,20 +14,31 @@ function sym(arg) {
     //turn args into array
     const args = [...arguments];
     //combine args into one array
-    const flatten = arr => arr.reduce((a, b) => {
-        
+    return args.reduce((arrA, arrB) => {
+        let symDiff = [];
+        arrA.forEach(function(item) {
+            if (arrB.indexOf(item) < 0 && symDiff.indexOf(item) < 0) {
+                symDiff.push(item);
+            }
+        });
+        arrB.forEach(function(item) {
+            if (arrA.indexOf(item) < 0 && symDiff.indexOf(item) < 0) {
+                symDiff.push(item);
+            }
+        });
+        console.log(symDiff);
+        return symDiff;
     });
-    return args;
 }
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+//sym([1, 2, 3], [5, 2, 1, 4]);
 
 //sym([1, 2, 3], [5, 2, 1, 4]); //should return [3, 4, 5].
 //sym([1, 2, 3], [5, 2, 1, 4]); //should contain only three elements.
 //sym([1, 2, 5], [2, 3, 5], [3, 4, 5]); //should return [1, 4, 5]
 //sym([1, 2, 5], [2, 3, 5], [3, 4, 5]); //should contain only three elements.
 //sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]); //should return [1, 4, 5].
-//sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]); //should contain only three elements.
+sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]); //should contain only three elements.
 //sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]); //should return [2, 3, 4, 6, 7].
 //sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]); //should contain only five elements.
 //sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]); //should return [1, 2, 4, 5, 6, 7, 8, 9].
