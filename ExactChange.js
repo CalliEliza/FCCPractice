adc7024d919762e1f6c99a6098c7ad0957bd17b2
callieliza <calidesigns42@gmail.com>
2017-02-09 18:08:28 -0500

    working on algorithm

--- ExactChange.js
+++ ExactChange.js
@@ -12,21 +12,28 @@
 //Otherwise, return change in coin and bills, sorted in highest to lowest order.
 
 function checkCashRegister(price, cash, cid) {
-    let change;
+    let change = [];
     let changeOwed = cash - price;
     let totalInDraw = 0;
     cid.forEach(coin => { //gets total about in draw
         totalInDraw+= coin[1];
     });
+    let drawObj = cid.reduce((curr, amt)  => {
+        curr.total += amt[1];
+        curr[amt[0]] = amt[1];
+        return curr;
+    }, {total: 0});
     // checks to see if amt in draw is less than or equal to the changed owed
-    if (Number(Math.round(totalInDraw+'e2')+'e-2') < changeOwed) {
+    if (drawObj.total < changeOwed) {
+        console.log("Insufficient Funds");
         return "Insufficient Funds";
     }
-    else if (Number(Math.round(totalInDraw+'e2')+'e-2') === changeOwed) {
+    else if (drawObj.total === changeOwed) {
+        console.log("Closed");
         return "Closed";
     }
-    console.log(totalInDraw);
-    console.log(Number(Math.round(totalInDraw+'e2')+'e-2'));
+
+    console.log(change);
     return change;
 }
 
@@ -42,3 +49,6 @@ function checkCashRegister(price, cash, cid) {
 // ["ONE HUNDRED", 100.00]]
 
 checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+//checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]); //should return a string.
+//checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]); //should return a string.
+//checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]); //should return "Insufficient Funds".
