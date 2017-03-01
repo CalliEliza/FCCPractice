d5fb17f332cb35d8f43b4c00617db5e3d243d899
callieliza <calidesigns42@gmail.com>
2017-03-01 17:02:31 -0500

    working on functionality of calc

--- advanced_projects/javascript_calculator/js/main.js
+++ advanced_projects/javascript_calculator/js/main.js
@@ -5,5 +5,17 @@
 const numbers = document.querySelectorAll('.key');
 const operations = document.querySelectorAll('.op');
 const screen = document.querySelector('#value');
+const clearAll = document.querySelector('#clearAll');
+const backspace = document.querySelector('#backspace');
 
 
+function performOperation(e) {
+    console.log(e);
+}
+
+
+numbers.forEach(number => number.addEventListener('click', () => {
+    screen.textContent = number;
+}));
+
+operations.forEach(operation => operation.addEventListener('click', performOperation));
