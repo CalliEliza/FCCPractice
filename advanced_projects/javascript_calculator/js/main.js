f428aa108c19945e7b45b130779cf362cfdced6d
callieliza <calidesigns42@gmail.com>
2017-03-01 17:24:29 -0500

    adding event listeners, working clear, backspace buttons

--- advanced_projects/javascript_calculator/js/main.js
+++ advanced_projects/javascript_calculator/js/main.js
@@ -2,20 +2,34 @@
  * Created by chewy on 2/27/17.
  */
 
-const numbers = document.querySelectorAll('.key');
-const operations = document.querySelectorAll('.op');
+const numbers = document.querySelectorAll('[data-num]');
+const operations = document.querySelectorAll('[data-op]');
 const screen = document.querySelector('#value');
 const clearAll = document.querySelector('#clearAll');
 const backspace = document.querySelector('#backspace');
+let currentNumber;
 
 
 function performOperation(e) {
     console.log(e);
 }
 
-
+//listens and gets the clicked on number
 numbers.forEach(number => number.addEventListener('click', () => {
-    screen.textContent = number;
+    //console.log(number.dataset.num);
+    screen.textContent += number.dataset.num; // adds to textContent
+    currentNumber = number.dataset.num; //set this to current number
 }));
 
+// listens for clicks on backspace button
+backspace.addEventListener('click', () => {
+    screen.textContent = screen.textContent.substr(0,screen.textContent.length-1);
+});
+
+//listens for click on clearAll button
+clearAll.addEventListener('click', () => {
+    screen.textContent = " ";
+});
+
 operations.forEach(operation => operation.addEventListener('click', performOperation));
+
