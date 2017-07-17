988b4ae2cc0e8179e36c4cd8164a9daa70deaf2a
callieliza <calidesigns42@gmail.com>
2017-07-17 12:18:24 -0400

    setting up solution

---
+++ algorithms/MaptheDebris.js
@@ -0,0 +1,20 @@
+
+
+//Return a new array that transforms the element's average altitude into their orbital periods.
+
+//The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+  //  The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+  //  The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+
+// formula needed: T = 2(3.14)(squareRoot((earthRadius + avAlt)^3)/GM)
+
+
+function orbitalPeriod(arr) {
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
