024498e8e9fa98df8d57cb3724aa3edc514698a8
Callie Brooks <calidesigns42@gmail.com>
2017-07-17 11:54:52 -0400

    Update NoRepeats.js
    working on problem

--- algorithms/NoRepeats.js
+++ algorithms/NoRepeats.js
@@ -2,31 +2,49 @@
  * Created by chewy on 2/13/17.
  */
 
-// Return the number of total permutations of the provided string that don't have repeated consecutive letters. Assume that all characters in the provided string are each unique.
+// Return the number of total permutations of the provided string that don't have repeated consecutive letters.
+// Assume that all characters in the provided string are each unique.
 
-// For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
+// For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa),
+// but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
 
 
-/*var str = ???;
-permAlone(current position in original string, characters used already in original string, created string) {
-    if (current string is finished) {
-        print current string;
-    } else {
-        for (var i = 0; i < str.length; i++) {
-            if (str[i] has not been used) {
-                put str[i] into the current position of new string;
-                mark str[i] as used;
-                permAlone(current position in original string, characters used already in original string, created string);
-                remove str[i] as used because another branch in the tree for i + 1 will likely use it;
+
+function permAlone(str) {
+    //regex
+    var rx = /(.)\1+/g;
+
+    //split --> array of char
+    var ary = str.split('');
+    var perm = [];
+    var temp;
+
+    //0 if str contains same char
+    if (str.match(rx) !== null && str.match(rx)[0] === str) {
+        return 0;
+    }
+
+    //fxn to switch variables
+    function switchVars(idx1, idx2) {
+        temp = ary[idx1];
+        ary[idx1] = ary[idx2];
+        ary[idx2] = temp;
+    }
+
+    //get permutations
+    function getPerms(x) {
+        if (x === 1) {
+            perm.push(ary.join(''));
+        } else {
+            for (let i =0; i != x; ++i) {
+                getPerms(x - 1);
+                switchVars(x % 2 ? 0 : i, x - 1);
             }
         }
     }
-}
-permAlone(0, nothing used yet, empty new string (or array the same size as str));*/
 
+    getPerms(ary.length);
 
-function permAlone(currentPos, charUsed, str) {
-    return str;
 }
 
 permAlone('aab');
