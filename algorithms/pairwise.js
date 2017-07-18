9d0c6e74430c7ca64022b898051b2586e71c9062
callieliza <calidesigns42@gmail.com>
2017-07-18 11:38:12 -0400

    nonworking solution

--- algorithms/pairwise.js
+++ algorithms/pairwise.js
@@ -13,11 +13,38 @@
 
 
 function pairwise(arr, arg) {
-    return arg;
+    let total =0;
+    let trackIndx = [];
+    let currentIdx = 0;
+    let temp = arr[currentIdx];
+    let count = 0;
+    const length = arr.length;
+    function mainFxn()  {
+        arr.forEach(function(x) {
+            let sum = temp + x;
+            if (sum === arg) {
+                trackIndx.push(arr.indexOf(temp));
+                trackIndx.push(arr.indexOf(x));
+                total+= sum;
+            }
+            ++count;
+            if (count === length) {
+                temp = arr[currentIdx+1];
+                //mainFxn();
+                count = 0;
+            } 
+
+        });
+    }
+    if (total === 0) {
+        mainFxn();
+    }
+    console.log(total);
+    return total;
 }
 
 pairwise([1,4,2,3,0,5], 7); //should return 11.
-pairwise([1, 3, 2, 4], 4);  //should return 1.
-pairwise([1, 1, 1], 2); //should return 1.
-pairwise([0, 0, 0, 0, 1, 1], 1); //should return 10.
-pairwise([], 100); //should return 0.
+//pairwise([1, 3, 2, 4], 4);  //should return 1.
+//pairwise([1, 1, 1], 2); //should return 1.
+//pairwise([0, 0, 0, 0, 1, 1], 1); //should return 10.
+//pairwise([], 100); //should return 0.
